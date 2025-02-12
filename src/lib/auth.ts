f73f4bf49567b9090e123f167a04d698b9936a67
Jay-JYKim91 <jay.jykim91@gmail.com>
2025-02-12 20:40:58 +0900

    chore: add react-router-dom

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,9 @@ import { supabase } from './supabase'
 export const signInWithGoogle = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
+        options: {
+            redirectTo: `${window.location.origin}/dashboard`
+        }
     });
 
     return { data, error }
